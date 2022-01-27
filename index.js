c01bbf7d2b33e7cf49b66cfbd26ad03abd38baf8
crusadersaurabh <saurabhkumar291096@gmail.com>
2022-01-28 00:08:58 +0530

    till operators

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
-// NaN(Not-A-Number, its a global Object, its is under global scope)
+// TODO: NaN(Not-A-Number, its a global Object, its is under global scope)
 
-console.log("Hello" - 5);
+// console.log("Hello" - 5);
 
-var myNumber = 9431686387;
-var myName = "saurabh";
-console.log(isNaN(myNumber));
-console.log(isNaN(myName));
+// var myNumber = 9431686387;
+// var myName = "saurabh";
+// console.log(isNaN(myNumber));
+// console.log(isNaN(myName));
 
-if (isNaN(myName)) {
-  console.log("Entered number is not a number");
-} else {
-  console.log("is a number");
-}
+// if (isNaN(myName)) {
+//   console.log("Entered number is not a number");
+// } else {
+//   console.log("is a number");
+// }
+
+// console.log(isNumber(NaN == NaN));
+
+// TODO:Operators
+// Types:-
+// 1.Assignment Operators
+// 2.Arithmetic Operators
+// 3.Logical Operators
+// 4.String Operators
+// 5.Conditional Operators
+// 6.Comparison Operators
+
+// TODO:Swap 2 variables
+// a = 2;
+// b = 8;
+// c = b;
+// b = a;
+// a = c;
+
+// console.log(a);
+// console.log(b);
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(a);
+// console.log(b);
+
+// TODO:Diff between == & === : In == it evaluates & give boolean value bt in === it evaluates & also check datatypes.
+
+// TODO:WAP of LeapYear
+
+// var year = 2020;
+// if (year % 4 == 0) {
+//   console.log("Its a leap year " + year);
+// } else {
+//   console.log("its not a leap year");
+// }
