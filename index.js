6d8e2b263b3e738272f9a4097a5a0ac78b3b54d7
crusadersaurabh <saurabhkumar291096@gmail.com>
2022-01-30 00:31:19 +0530

    splice method

--- index.js
+++ index.js
@@ -50,3 +50,53 @@
 // } else {
 //   console.log("its not a leap year");
 // }
+
+// #TODO:Print table of 8
+
+// var num = 8;
+// for (i = 1; i <= 10; i++) {
+//   var product = num * i;
+//   console.log(product);
+// }
+
+// TODO:Anonamouys function
+
+// var exp = function (a, b) {
+//   total = a + b;
+//   return total;
+// };
+// var sum = exp(10, 20);
+// console.log(sum);
+
+// TODO:Traversal in Array
+
+// var myFriends = ["Rameh", "Suresh", "Rajesh", "ACN"];
+
+// for (i = 0; i <= myFriends.length - 1; i++) {
+//   console.log(myFriends[i]);
+// }
+// #TODO: for in loop & for of loop
+
+// var myFriends = ["Rameh", "Suresh", "Rajesh", "ACN"];
+// for (let elements in myFriends) {
+//   console.log(elements);
+// }
+
+// for (let ele of myFriends) {
+//   console.log(ele);
+// }
+
+// TODO: .forEach loop
+
+// var myFriends = ["Ram", "Shyam", "Raaan", "Ranjiii"];
+
+// myFriends.forEach(function (Element, index, array) {
+//   console.log(Element);
+// });
+
+// TODO: splice method
+
+// const myFriends = ["Ram", "jig", "lota", "man"];
+// console.log(myFriends);
+// var myNewFriends = myFriends.splice(0, 0, "lemon");
+// console.log(myFriends);
